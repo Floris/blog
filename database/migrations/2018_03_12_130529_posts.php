d228280f72c2464a4cdfb72f2c956f16940ad895
Floris Droppert <21892@ma-web.nl>
2018-06-06 14:43:27 +0200

    bug fix :hammer:

--- database/migrations/2018_03_12_130529_posts.php
+++ database/migrations/2018_03_12_130529_posts.php
@@ -14,11 +14,11 @@ class Posts extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
+             $table->increments('id');
             $table->char('title');
             $table->MEDIUMTEXT('post_content');
-            $table->integer('draft');
-            $table->char('tags');
+            $table->integer('draft')->nullable();
+            $table->char('tags')->nullable();
             $table->timestamps();
         });
     }
